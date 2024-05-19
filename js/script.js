55b53cc89c1b7154fe9752accb764c649fbc2800
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-19 13:56:22 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Jack LeVasseur
-// Created on: April 10 2024
+// Created by: Jack
+// Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-"use strict"
-
 /**
- * This function calculates area of a triangle.
+ * This function converts Fahrenheit to Celsius
  */
-function calculatePermeterAndAreaOfRectangle() {
+function convertFahrenheitToCelsius() {
   // input
-  const baseOfTriangle = parseInt(document.getElementById('length-of-rectangle').value)
-  const heightOfTriangle = parseInt(document.getElementById('width-of-rectangle').value)
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit-input').value);
 
   // process
-  const areaOfTriangle = baseOfTriangle + heightOfTriangle - 80
-  const HeightOfTriangle = (baseOfTriangle + heightOfTriangle) - 80 
+  const celsius = ((fahrenheit - 32) * 5 / 9).toFixed(3);
 
   // output
-  document.getElementById('area').innerHTML = 'Celsius is 26.667°: ' + areaOfTriangle 
-  document.getElementById('perimeter').innerHTML = 'Celsius is 26.667°: ' + perimeterOfRectangle 
+  document.getElementById('celsius-result').innerHTML = 'Celsius:' + celsius + '°';
 }
